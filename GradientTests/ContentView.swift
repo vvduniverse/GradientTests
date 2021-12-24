cddf4abf6251d44fbb6fea2f69d7f2f1c339a251
Vahtee Boo <ronnie.kaa@bigmir.net>
2021-12-24 22:37:07 +0200

    yes

--- GradientTests/ContentView.swift
+++ GradientTests/ContentView.swift
@@ -9,8 +9,37 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        VStack(spacing: 1) {
+            LinearGradient(colors: [.red, .green, .purple], startPoint: .topLeading, endPoint: .bottomTrailing)
+            
+            LinearGradient(colors: [.white, .black], startPoint: .top, endPoint: .bottom)
+            
+//            Gradient(colors: [.yellow, .green, .blue], startPoint: .topLeading, endPoint: .bottomTrailing)
+            
+            LinearGradient(gradient: Gradient(colors: [.white, .black]), startPoint: .top, endPoint: .bottom)
+            
+            LinearGradient(gradient: Gradient(stops: [
+                    Gradient.Stop(color: .white, location: 0.15),
+                    Gradient.Stop(color: .black, location: 0.85),
+                ]), startPoint: .top, endPoint: .bottom)
+            
+            LinearGradient(gradient: Gradient(stops: [
+                    .init(color: .white, location: 0.45),
+                    .init(color: .black, location: 0.55),
+                ]), startPoint: .top, endPoint: .bottom)
+            
+            LinearGradient(stops: [.init(color: .green, location: 0.2),
+                                    .init(color: .yellow, location: 0.5),
+                                    .init(color: .black, location: 0.8)],
+                           startPoint: .leading, endPoint: .trailing)
+            
+            RadialGradient(gradient: Gradient(colors: [.blue, .black]), center: .center, startRadius: 20, endRadius: 200)
+            
+            Circle()
+                .fill(AngularGradient(gradient: Gradient(colors: [.red, .yellow, .green, .blue, .purple, .red]), center: .center))
+                .frame(width: 180, height: 180)
+               
+        }
     }
 }
 
