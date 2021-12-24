db560953e3eb6f783b6e4fd47ef9f8243d025463
Vahtee Boo <ronnie.kaa@bigmir.net>
2021-12-24 20:57:03 +0200

    Initial Commit

---
+++ GradientTests/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  GradientTests
+//
+//  Created by Vahtee Boo on 24.12.2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ GradientTests/GradientTestsApp.swift
@@ -0,0 +1,17 @@
+//
+//  GradientTestsApp.swift
+//  GradientTests
+//
+//  Created by Vahtee Boo on 24.12.2021.
+//
+
+import SwiftUI
+
+@main
+struct GradientTestsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
